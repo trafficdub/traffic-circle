232617a7b118e2263e2e82cd5418f28c6e31fdfc
trafficdub <trafficdub@users.noreply.github.com>
2020-11-10 10:26:56 -0800

    Modify index view

--- traffic_circle/polls/views.py
+++ traffic_circle/polls/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, you are are the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, "polls/index.html", {
+        'latest_question_list': latest_question_list,
+    })
+
+def detail(request, question_id):
+    return HttpResponse(f"Details of question: {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"Results of question: {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Voting on question: {question_id}.")
