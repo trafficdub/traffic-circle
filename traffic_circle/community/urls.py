0c8706af08d78bd24893b069ec3ecc3262ea95a4
trafficdub <trafficdub@users.noreply.github.com>
2021-07-30 01:20:15 -0700

    Add form for submitting new post

--- traffic_circle/community/urls.py
+++ traffic_circle/community/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "community"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.user, name="user")
 ]
--- traffic_circle/community/views.py
+++ traffic_circle/community/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
+from django import forms
+from django.urls import reverse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+
+posts = ["Post1", "Post2", "Post3"]
+
+class NewPostForm(forms.Form):
+    post = forms.CharField(label="New Post", min_length=1, max_length=100)
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "community/index.html", {
+        "posts": posts
+    })
+
+def user(request, name):
+    form = NewPostForm()
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            post = form.cleaned_data["post"]
+            posts.append(post)
+            return HttpResponseRedirect(reverse("community:user", args=[name]))
+    return render(request, "community/user.html", {
+        "name": name.capitalize(),
+        "form": form
+    })
